979065dac7bf90dbfe9164771583fcdd8698bb03
MEC <mistereliteclan@gmail.com>
2016-01-06 16:39:28 +0100

    frame

--- Immune-Warfare/ImmuneWarfare.java
+++ Immune-Warfare/ImmuneWarfare.java
@@ -6,4 +6,19 @@ import java.util.*;
 
 public class ImmuneWarfare extends JFrame{
     
+    public static final int PL = 0; //playing
+    public static final int WI = 1; //win
+    public static final int LO = 2; //lost
+    public static final int PA = 3; //pause
+    public static final int ME = 4; //menu
+    public static final int DI = 5; //dialog
+    
+    public void Game(){
+        setTitle ("Immune Warefare");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,700);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
 }
