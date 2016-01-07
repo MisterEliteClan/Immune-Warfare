c133e72ac51cbb55c3f0cf8e818ea77528ad342c
MEC <mistereliteclan@gmail.com>
2016-01-07 20:54:59 +0100

    srfsg
    sgdgsd

--- Immune-Warfare/Board.java
+++ Immune-Warfare/Board.java
@@ -3,24 +3,65 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.*;
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import javax.swing.Timer;
 
+public class Board extends JPanel implements KeyListener, ActionListener, Commons {
 
-public class Board extends JPanel implements Commons {
-
+    Timer tm = new Timer(5,this);
+    public static boolean SPACE, UP, DOWN;
     private boolean ingame;
-    
-    private boolean UP = Navi.UP; 
-    private boolean DOWN = Navi.DOWN;
-    private boolean SPACE = Navi.SPACE;
-    
     private Dimension d;
-    private Navi navi;
-    
     public static int state = ME;
+    
+    public void actionPerformed(ActionEvent e){repaint();}
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        
+        if (key == KeyEvent.VK_UP){
+            UP = true;
+        }
 
+        if (key == KeyEvent.VK_DOWN){
+            DOWN = true;
+        }
+        
+        if (key == KeyEvent.VK_SPACE){
+            SPACE = true;
+        }
+    }
+
+    public void keyTyped(KeyEvent e){}
+    
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_UP){
+            UP = false;
+        }
+
+        if (key == KeyEvent.VK_DOWN){
+            DOWN = false;
+        }
+        
+        if (key == KeyEvent.VK_SPACE){
+            SPACE = false;
+        }
+    }
+    
     public Board() {
         d = new Dimension(BO_WI, BO_HE);
         setBackground(Color.black);
+        tm.start();
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
     } 
     
     public void paint(Graphics g){
@@ -68,17 +109,6 @@ public class Board extends JPanel implements Commons {
                 System.out.println("interrupted");
             }
             beforeTime = System.currentTimeMillis();
-        }
-    }
-    
-    private class TAdapter extends KeyAdapter {
-        
-        public void keyPressed(KeyEvent e) {
-            navi.keyPressed(e);
-        }
-
-        public void keyReleased(KeyEvent e) {
-            navi.keyReleased(e);
         }
     }
 }
--- Immune-Warfare/Navi.java
+++ Immune-Warfare/Navi.java
@@ -1,9 +1,26 @@
-import java.awt.event.KeyEvent;
+/*import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
-public class Navi implements Commons{
-    
+import javax.swing.Timer;
+
+public class Navi extends JPanel implements KeyListener, ActionListener, Commons{
+    Timer tm = new Timer(5,this);
     public static boolean SPACE, UP, DOWN;
-
+    
+    public Navi(){
+    
+        tm.start();
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        
+    }
+    
+    public void actionPerformed(ActionEvent e){}
+    
+    @Override
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         
@@ -20,6 +37,9 @@ public class Navi implements Commons{
         }
     }
 
+    public void keyTyped(KeyEvent e){}
+    
+    @Override
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
 
@@ -36,3 +56,4 @@ public class Navi implements Commons{
         }
     }
 }
+*/
